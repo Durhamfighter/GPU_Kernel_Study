234b2fa06ce35026f51d29ca80bd98f856d23533
SEUNGHUN SHIN <sinseunghun708@gmail.com>
2026-01-06 10:59:30 +0900

    Feat : finished day1

--- day1/cuda/day1.cu
+++ day1/cuda/day1.cu
@@ -1,1 +1,57 @@
 // cuda day1
+#include <cuda_runtime.h>
+#include <stdio.h>
+
+__global__ void add_kernel(float *x,float *y,float *z,int n_elements){
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (idx<n_elements){
+        z[idx] = x[idx] + y[idx];
+    }
+}
+
+int main(){
+    const int n_elements = 1000;
+    const int size = n_elements * sizeof(float);
+    
+    // Host arrays
+    float *h_x = (float*)malloc(size);
+    float *h_y = (float*)malloc(size);
+    float *h_z = (float*)malloc(size);
+
+    for (int i=0;i<1000;i++){
+        h_x[i] = i;
+        h_y[i] = i;
+    }
+    //device_arrays
+    float *d_x,*d_y,*d_z;
+    cudaMalloc((void**)&d_x,size);
+    cudaMalloc((void**)&d_y,size);
+    cudaMalloc((void**)&d_z,size);
+
+    cudaMemcpy(d_x,h_x,size,cudaMemcpyHostToDevice);
+    cudaMemcpy(d_y,h_y,size,cudaMemcpyHostToDevice);
+
+    dim3 blockDim(256,1,1);
+    dim3 gridDim((n_elements + blockDim.x - 1) / blockDim.x,1,1);
+
+    add_kernel<<<gridDim,blockDim>>>(d_x,d_y,d_z,n_elements);
+
+    cudaDeviceSynchronize();
+
+    cudaMemcpy(h_z,d_z,size,cudaMemcpyDeviceToHost);
+
+    for (int i=0;i<10;i++){
+        printf("%f\n",h_z[i]);
+    }
+
+    //clean
+    free(h_x);
+    free(h_y);
+    free(h_z);
+    cudaFree(d_x);
+    cudaFree(d_y);
+    cudaFree(d_z);
+
+    return 0;
+}
--- day1/triton/day1.py
+++ day1/triton/day1.py
@@ -1,2 +1,33 @@
 # triton day1
-import triton
+import triton 
+import triton.language as tl
+import torch
+
+
+@triton.jit
+def add_kernel(x_ptr,y_ptr,z_ptr,n_elements,BLOCK_SIZE:tl.constexpr):
+    block_idx = tl.program_id(0)
+
+    block_start = block_idx * BLOCK_SIZE
+
+    offsets = block_start + tl.arange(0,BLOCK_SIZE)
+
+    mask = offsets < n_elements
+
+    x = tl.load(x_ptr + offsets,mask=mask)
+    y = tl.load(y_ptr + offsets,mask=mask)
+    z = x + y
+
+    tl.store(z_ptr + offsets,z,mask=mask)
+
+if __name__ == "__main__":
+    n_elements = 1000
+    BLOCK_SIZE = 256
+
+    x = torch.randn(n_elements,dtype=torch.float32,device='cuda')
+    y = torch.randn(n_elements,dtype=torch.float32,device='cuda')
+    z = torch.zeros(n_elements,dtype=torch.float32,device='cuda')
+    grid = ((n_elements + BLOCK_SIZE - 1) // BLOCK_SIZE,)
+    add_kernel[grid](x,y,z,n_elements,BLOCK_SIZE)
+
+    print(z)
