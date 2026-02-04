a4a0f0ef48a0a694c25603fc1381120c2bf10133
SEUNGHUN SHIN <sinseunghun708@gmail.com>
2026-02-04 23:20:39 +0900

    day19

--- day19/triton/day19.py
+++ day19/triton/day19.py
@@ -1,7 +1,8 @@
 import triton 
 import triton.language as tl 
 import torch
-
+def tl_round(x):
+    return tl.where(x >= 0, tl.floor(x + 0.5), tl.ceil(x - 0.5))
 @triton.jit
 def weight_quant_int8(weight_ptr,weight_stride,q_weight_ptr,q_weight_stride,num_cols,scale_ptr,BLOCK_SIZE : tl.constexpr):
     row = tl.program_id(axis=0)
@@ -9,15 +10,21 @@ def weight_quant_int8(weight_ptr,weight_stride,q_weight_ptr,q_weight_stride,num_
     mask = col_off<num_cols
     weight = tl.load(weight_ptr + row*weight_stride+col_off , mask = mask)
     scale = tl.load(scale_ptr + row)
-    q_weight = tl.maximum(tl.minimum(tl.round(weight/scale),127),-127).to(tl.int8) # int 8
+    q = tl.where(
+        weight >= 0,
+        tl.floor(weight + 0.5),
+        tl.ceil(weight - 0.5),
+    )
+    q_weight = tl.maximum(tl.minimum(q,127),-127).to(tl.int8) # int 8
     tl.store(q_weight_ptr + row*q_weight_stride + col_off,q_weight,mask = mask)
 
 if __name__=='__main__':
-    weight = torch.randn(352,1000,dtype=torch.float32,dtype='device')
-    scale = [max(abs(row))/127 for row in weight]
-    q_weight = torch.empty(352,1000,dtype=torch.int8,dtype='device')
+    weight = torch.randn(352,1000,dtype=torch.float32,device='cuda')
+    scale = weight.abs().max(dim=1).values / 127.0
+    q_weight = torch.empty(352,1000,dtype=torch.int8,device='cuda')
     row,col = weight.shape
     BLOCK_SIZE = triton.next_power_of_2(col)
     
     grid = (row,1,1)
-    weight_quant_int8[grid](weight,q_weight,)
+    weight_quant_int8[grid](weight,weight.stride(0),q_weight,q_weight.stride(0), col,scale,BLOCK_SIZE)
+    print(q_weight)
